b707eebd98ddf0f99f704d7338616075d1e20d3c
marilena <marilena.daquino2@unibo.it>
2020-06-25 12:35:28 +0200

    added publications and news

--- contact_process.php
+++ contact_process.php
@@ -1,6 +1,6 @@
 <?php
 
-    $to = "rockybd1995@gmail.com";
+    $to = "marilena.daquino2@unibo.it";
     $from = $_REQUEST['email'];
     $name = $_REQUEST['name'];
     $subject = $_REQUEST['subject'];
